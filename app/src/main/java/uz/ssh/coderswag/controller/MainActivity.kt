f4f622dc612cd2a3d94abb47db41a793b35b02fc
ShohimardonDev <abdurashitovsh19@gmail.com>
2024-03-19 22:06:50 +0500

    ✨ Add toString method to Category.kt and update UI in MainActivity.kt and activity_main.xml

--- app/src/main/java/uz/ssh/coderswag/controller/MainActivity.kt
+++ app/src/main/java/uz/ssh/coderswag/controller/MainActivity.kt
@@ -1,15 +1,26 @@
 package uz.ssh.coderswag.controller
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import uz.ssh.coderswag.R
+import uz.ssh.coderswag.model.Category
+import uz.ssh.coderswag.service.DataService
 
 class MainActivity : AppCompatActivity() {
+    lateinit var adapter: ArrayAdapter<Category>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(
+            this, android.R.layout
+                .simple_list_item_1, DataService.categorys
+        )
+        findViewById<ListView>(R.id.catogoryListView).adapter = adapter
 
     }
 }
--- app/src/main/java/uz/ssh/coderswag/model/Category.kt
+++ app/src/main/java/uz/ssh/coderswag/model/Category.kt
@@ -1,4 +1,8 @@
 package uz.ssh.coderswag.model
 
 class Category(var title: String, var image: String) {
+    override fun toString(): String {
+
+        return title;
+    }
 }
